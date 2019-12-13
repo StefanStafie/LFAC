8b92c69c40ccce44c5de62faee848cca9e62b8d7
amaterasuonpc <43759618+amaterasuonpc@users.noreply.github.com>
2019-12-13 23:59:15 +0200

    Add files via upload

---
+++ mask.h
@@ -0,0 +1,12 @@
+typedef struct expr_info {
+	int intvalue;   //1
+        char* strvalue;  //2
+	_Bool boolvalue; //3
+	char charvalue; //4
+	float floatvalue; //5
+	int type;
+	char* path;
+} expr_info;
+
+
+
